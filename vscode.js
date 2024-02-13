de677ef1d450da01b3f5282b40a646799483cb6b
Albert-S2 <albert733844@gmail.com>
2024-02-13 23:47:43 +0000

    day8-4

--- vscode.js
+++ vscode.js
@@ -1,67 +1,35 @@
-
-
 /*
-if (false) {
-    console.log("Grrat News, you did it!");
-} else {
-    console.log("I am sorry, you need to start again);")
+
+const todolist = [
+   'make dinner',
+   'wash dishes',
+   'watch youtube'
+];
+ for (let index = 0; index <= todolist.length - 1; index++) {
+   const value = todolist[index];
+   console.log(value);
+
  }
 
- const age = 29;
- if (age >= 16) {
-    console.log('You can drive');
- } else if (age >= 14) {
-    console.log('You are almost there');
- } else {
-    console.log('You cannot drive');
+*/
+
+const nums = [20, 35, 46]
+let total = 0
+
+for (let i = 0; i < nums.length; i++) {
+   const num = nums [i];
+   total += num; 
+}; 
+console.log(total);
+
+
+let numsdoubled = [];
+ for (let i = 0; i < nums.length; i++) {
+   const num = nums[i];
+   numsdoubled.push(num * 2);
  }
 
- let cartQuantity = 0;
-
- if (cartQuantity > 0) {
-    console.log("You have items in your cart");
- } else {
-    console.log("Your cart is empty");
- }
- */
-
- let myArray = [10, 20, 30];
- console.log(myArray);
-
- myArray [0] = 99;
- console.log(myArray);
-
- console.log(myArray.length);
-
- myArray.push (100);
- console.log(myArray);
-
- myArray.splice(1, 1);
- console.log(myArray);
-
-let i = 1;
-
-while (i <= 5) {
-    console.log(i);
-    i = i + 1;
-}
-
-for (let r = 1; r <= 5; r++) {
-   console.log(r);
-}
-
-let randomNumber = 0;
-while (randomNumber < 0.5) 
-   randomNumber = Math.random();
-
-console.log(randomNumber);
-
-
-
-
-
-
-
+ console.log(numsdoubled); 
 
 
 
