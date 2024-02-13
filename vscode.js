4e633c6043aa731f71575891b10974775ece25e8
Albert-S2 <albert733844@gmail.com>
2024-02-13 22:52:22 +0000

    day8-2

--- vscode.js
+++ vscode.js
@@ -1,6 +1,6 @@
 
 
-
+/*
 if (false) {
     console.log("Grrat News, you did it!");
 } else {
@@ -15,3 +15,56 @@ if (false) {
  } else {
     console.log('You cannot drive');
  }
+
+ let cartQuantity = 0;
+
+ if (cartQuantity > 0) {
+    console.log("You have items in your cart");
+ } else {
+    console.log("Your cart is empty");
+ }
+ */
+
+ let myArray = [10, 20, 30];
+ console.log(myArray);
+
+ myArray [0] = 99;
+ console.log(myArray);
+
+ console.log(myArray.length);
+
+ myArray.push (100);
+ console.log(myArray);
+
+ myArray.splice(1, 1);
+ console.log(myArray);
+
+let i = 1;
+
+while (i <= 5) {
+    console.log(i);
+    i = i + 1;
+}
+
+for (let r = 1; r <= 5; r++) {
+   console.log(r);
+}
+
+let randomNumber = 0;
+while (randomNumber < 0.5) 
+   randomNumber = Math.random();
+
+console.log(randomNumber);
+
+
+
+
+
+
+
+
+
+
+
+
+
