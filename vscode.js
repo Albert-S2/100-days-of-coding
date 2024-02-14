3a753c0798093fcdd83e8c44c64dd60516e1275d
Albert-S2 <albert733844@gmail.com>
2024-02-14 21:37:07 +0000

    Day9-2

--- vscode.js
+++ vscode.js
@@ -13,26 +13,32 @@ const todolist = [
 
 */
 
-const nums = [20, 35, 46]
-let total = 0
+// const nums = [20, 35, 46]
+// let total = 0
 
-for (let i = 0; i < nums.length; i++) {
-   const num = nums [i];
-   total += num; 
-}; 
-console.log(total);
-
-
-let numsdoubled = [];
- for (let i = 0; i < nums.length; i++) {
-   const num = nums[i];
-   numsdoubled.push(num * 2);
- }
-
- console.log(numsdoubled); 
+// for (let i = 0; i < nums.length; i++) {
+//    const num = nums [i];
+//    total += num; 
+// }; 
+// console.log(total);
 
 
+// let numsdoubled = [];
+//  for (let i = 0; i < nums.length; i++) {
+//    const num = nums[i];
+//    numsdoubled.push(num * 2);
+//  }
 
+//  console.log(numsdoubled); 
 
+// Happy Valentine's Day!
+const love = (you, me) => {
+  let forever = true;
+  while (forever) {
+    console.log(`I ❤️ ${you}`);
+    console.log(`And ${you} ❤️ me`);
+    console.log(`Together, we make a perfect team!`);
+  }
+};
 
-
+love("JavaScript", "Coder");
