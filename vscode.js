3894eb6ed3cb52aba8fa450d6b70ae210d470eae
Albert-S2 <albert733844@gmail.com>
2024-02-18 22:09:38 +0000

    day13-4

--- vscode.js
+++ vscode.js
@@ -1,13 +1,86 @@
+// function greed(name, time){
+// console.log(`Good ${time}, ${name}`); 
+// }
 
-let priceGBP = [i]; 
+// greed('Albert', 'morning');
+
+// function maths(goal){
+//     let newGoal = 25 * goal + 45;
+//     return newGoal
+// }
+
+// let number3 = maths(23);
+// console.log(number3); 
 
 
 
-function convertGBPtoEUR(a, b) {
-    let priceEUR = a 
-    
-    return b 
+// function priceEuro(basic){
+//     let newPrice = basic * 1.17; 
+//     return newPrice
+// }
+
+// let convertedPrice = priceEuro(67);
+// console.log(`The price in EURO is ${convertedPrice}`); 
+
+
+// function priceUSD(basic2) {
+//         let newUSDPrice = basic2 * 1.26;
+//         return newUSDPrice
+// }
+
+// let convertedUSDPrice = priceUSD(45);
+// console.log(`The price in USD is ${convertedUSDPrice}`); 
+
+
+// function pricePLN (basic3) {
+//     let newPricePLN = basic3 * 5.07; 
+//     return newPricePLN
+// }
+
+// let convertedPLNPrice = pricePLN(40); 
+// console.log(`The price in PLN: ${convertedPLNPrice}`); 
+
+
+
+// function priceCHF(basic4) {
+//     let newCHFPrice = basic4 * 1.11;
+//     return newCHFPrice
+// }
+
+// let convertedCHF = priceCHF(50); 
+// console.log(`Price in CHF: ${convertedCHF}`); 
+
+// let prices = [12, 32, 45, 65, 78]; 
+
+// function pricesEURO(basic5) {
+//     let total = []; 
+//     for(let i = 0; i < pricesEURO.length; i++) {
+//         total = prices[i] * 1.17;
+//         return total
+//     }
+// }
+
+// let exchangedEURO = pricesEURO(prices); 
+// console.log(`new prices: ${exchangedEURO}`); 
+
+
+
+
+// function temperatureExchange(newT){
+//     let temperature2 = newT * 1.8 + 32
+//     return temperature2
+// }
+// let farenheit = temperatureExchange(25)
+// console.log(`In Farenheit it is ${farenheit} degrees`); 
+
+
+
+
+
+
+function its100daysChallenge(day) {
+    let whichDay = day + 13;
+    return whichDay
 }
-
-console.log(`For a price of ${priceGBP} 
- GBP, it is ${} in EUR.`); 
+let theDay = its100daysChallenge(0); 
+console.log(`It is my ${theDay}th day in the challenge!`); 
