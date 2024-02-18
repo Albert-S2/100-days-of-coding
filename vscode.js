9f9f5e71b2169fe1fbd414890c12231ee76f6714
Albert-S2 <albert733844@gmail.com>
2024-02-18 19:10:17 +0000

    day13-3

--- vscode.js
+++ vscode.js
@@ -1,27 +1,13 @@
-console.log(23 + 97);
-console.log(6 + 5 + 8 + 10 + 4 + 9);
-console.log((4 + 6 + 9) / 77);
 
-let a = 10
-console.log(a);
+let priceGBP = [i]; 
 
-let message = "Tomorrow is Sunday"; 
-console.log(message);
 
-let number = 24
-let number2 = 76
 
-console.log(`my favorite number is ${number + number2}`); 
+function convertGBPtoEUR(a, b) {
+    let priceEUR = a 
+    
+    return b 
+}
 
-let abc = 56
-let bcc = 34
-let ccba = abc + bcc; 
-
-console.log(`Thomas run yesterday ${(ccba + abc) / 1000} kilometers`)
-
-let MAX = 57
-let actual = MAX - 13
-let percentage = actual / MAX
-
-console.log(percentage)
-
+console.log(`For a price of ${priceGBP} 
+ GBP, it is ${} in EUR.`); 
